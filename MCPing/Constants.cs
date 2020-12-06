18abeda4cc53651d3c86c45c8bd0229d6e6c5ea6
Syphini <syphinigamer@gmail.com>
2020-12-07 00:58:04 +1100

    Updated Functions

---
+++ MCPing/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCPing
+{
+    public class Constants
+    {
+        public const string ipListPath = "config/iplist.json";
+        public const string namesPath = "config/names.json";
+        public const string scannedPath = "config/scanned.json";
+        public const string serverListPath = "config/serverList.json";
+    }
+}
