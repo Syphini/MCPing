0817f347824a44c182843dfcf671a218369fc4a5
Mike Bravo <syphinigamer@gmail.com>
2020-12-28 15:38:29 +1100

    Function Update

---
+++ MCPing/Client.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MCPing
+{
+    using static Functions;
+    class Client
+    {
+        public TcpClient client;
+        private Packet packet;
+        private byte[] buffer;
+
+        public Client(TcpClient _client)
+        {
+            client = _client;
+
+            packet = new Packet(_client.GetStream());
+
+            buffer = new byte[short.MaxValue];
+            packet.stream.BeginRead(buffer, 0, buffer.Length, StreamCallback, null);
+        }
+
+        void StreamCallback(IAsyncResult result)
+        {
+            try
+            {
+                int byteLength = packet.stream.EndRead(result);
+                if (byteLength <= 0)
+                {
+                    return;
+                }
+
+                int value = packet.ReadInt(buffer);
+
+                Console.WriteLine(value);
+
+                packet.stream.BeginRead(buffer, 0, buffer.Length, StreamCallback, null);
+            }
+            catch (Exception ex)
+            {
+                ThrowError(packet.ip, $"TCP Read Error", ex);
+            }
+        }
+    }
+}
