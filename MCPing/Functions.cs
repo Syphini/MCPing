68596034e5020bd42581b88332c13b55a5ff90a4
Mike Bravo <syphinigamer@gmail.com>
2020-12-28 15:38:29 +1100

    Function Update

---
+++ MCPing/Functions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCPing
+{
+    public static class Functions
+    {
+        public static void ThrowError(string ip, string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{ip} ---- {message}");
+            Console.ResetColor();
+        }
+
+        public static void ThrowError(string ip, string message, Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{ip} ---- {message}: \n{ex}");
+            Console.ResetColor();
+        }
+    }
+}
